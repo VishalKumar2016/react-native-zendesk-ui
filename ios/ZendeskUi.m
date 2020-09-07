20087427c5a8771411ecef2ce98b36857d6be700
Vishal Kumar <vishalkumar7458@gmail.com>
2020-09-07 17:05:42 +0530

    Create method in iOS

--- ios/ZendeskUi.m
+++ ios/ZendeskUi.m
@@ -1,13 +1,27 @@
 #import "ZendeskUi.h"
+#import <SupportSDK/SupportSDK.h>
+#import <ZendeskCoreSDK/ZendeskCoreSDK.h>
 
 @implementation ZendeskUi
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(initZendesk:(NSString *)zendeskUrl zendeskAppId:(NSString *)appId zendeskClientId:(NSString *)clientId)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    [ZDKZendesk initializeWithAppId:appId clientId:clientId zendeskUrl: zendeskUrl];
+    [ZDKSupport initializeWithZendesk:[ZDKZendesk instance]];
+
+    id<ZDKObjCIdentity> userIdentity = [[ZDKObjCAnonymous alloc] initWithName:nil email:nil];
+    [[ZDKZendesk instance] setIdentity:userIdentity];
+}
+
+RCT_EXPORT_METHOD(showHelpCenter)
+{
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        UIViewController *helpCenterUI = [ZDKHelpCenterUi buildHelpCenterOverviewUi];
+         UINavigationController *navController  = [[[UIApplication sharedApplication] keyWindow] rootViewController];
+        [navController pushViewController:helpCenterUI animated:true];
+    }];
 }
 
 @end
